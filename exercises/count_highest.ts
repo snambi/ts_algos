54f8dbafdd4881940c8a4a41adcde8b26cfad650
Nambi Sankaran <snambi@gmail.com>
2025-07-24 15:33:04 -0700

    initial commit

---
+++ exercises/count_highest.ts
@@ -0,0 +1,13 @@
+
+export class HCounter {
+    count(input: number[]) : number {
+        
+        let count=0;
+        let highest=0;
+
+        for( let x in input ){
+           console.log(" value: "+ x )
+        }
+        return count;
+    }
+}
---
+++ index.ts
@@ -0,0 +1,14 @@
+
+import { HCounter } from "./exercises/count_highest";
+console.log("Hello via Bun!");
+
+let counter = new HCounter();
+
+let x = [2,3,4,8,5,4,3];
+
+console.log( "count = " + counter.count(x) );
+
+
+
+
+
